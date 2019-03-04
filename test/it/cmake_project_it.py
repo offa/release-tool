3e610fd2e099be9654e4e900a16305c3bd538ec2
offa <bm-dev@yandex.com>
2019-03-04 21:36:36 +0100

    CMake IT added.

--- test/it/cmake_project_it.py
+++ test/it/cmake_project_it.py
@@ -1,9 +1,14 @@
+from release_tool.cmake import CMakeProject
 import unittest
 
 class CMakeProjectIT(unittest.TestCase):
 
     def test_loading_project_file(self):
-        pass
+        proj = CMakeProject('test/it/data')
+        proj.load()
+
+        self.assertEqual(proj.name(), 'IntegrationTest')
+        self.assertEqual(proj.current_version(), '1.8.9')
 
 
 
