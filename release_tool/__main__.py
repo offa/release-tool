96559b27afbd2500378d53700524c9a389793548
offa <bm-dev@yandex.com>
2019-03-05 18:56:44 +0100

    Error checks added, refactoring and code cleanup.

--- release_tool/__main__.py
+++ release_tool/__main__.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import git
 from .cmake import CMakeProject
@@ -6,7 +7,7 @@ def parse_args():
     parser = argparse.ArgumentParser(prog="release-tool",
                                      description='Performs releases')
     required = parser.add_argument_group('arguments')
-    required.add_argument('--release-version', '-r', type=str,
+    required.add_argument('--release-version', '-r', type=str, required=True,
                           help='Version to release')
 
     parser.add_argument('--version', '-v', action='version', version="%(prog)s 0.0.1",
@@ -16,36 +17,34 @@ def parse_args():
     return parser.parse_args()
 
 
+def ensure_condition(condition, message):
+    if not condition:
+        print("{}".format(message))
+        sys.exit(1)
+
+
 def main():
     args = parse_args()
 
-    if not args.release_version:
-        print("ERROR: No Version")
-        return
-
     path = args.path[0]
     repo = git.Repo(path)
-
-    # Check Dirty
-    if repo.is_dirty():
-        print("ERROR: SCM Dirty")
-        return # TODO: rtn code
-
-    # Update Version
     proj = CMakeProject(path)
     proj.load()
+
+    ensure_condition(not repo.is_dirty(), 'The project contains uncommited changes')
+    ensure_condition(proj.version() != args.release_version, "Version already up-to-date")
+
+    # Update Version
     proj.set_version(args.release_version)
     proj.store()
 
 
     # Commit
-    repo.index.add(["CMakeLists.txt"])
+    repo.index.add([proj.project_config()])
+    ensure_condition(repo.is_dirty(), 'No changes to commit')
     repo.index.commit("Release v{}".format(proj.version()))
-    # Tag
     repo.create_tag("v{}".format(proj.version()), message="Release v{}".format(proj.version()))
 
-
-    # Done
 
 
 if __name__ == '__main__':
