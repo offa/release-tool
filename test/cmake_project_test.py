b984cb701525b815eefd27aaa58019996cf5ef51
offa <bm-dev@yandex.com>
2019-03-06 18:35:21 +0100

    Import order fixed.

--- test/cmake_project_test.py
+++ test/cmake_project_test.py
@@ -15,9 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from release_tool.cmake import CMakeProject
 import unittest
 from unittest.mock import patch
+from release_tool.cmake import CMakeProject
 
 
 class TestCMakeProject(unittest.TestCase):
